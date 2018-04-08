91fcfbdb9e7e603a9cf9dc27c3c07f89b6db59ae
Sihan Wang <shwang95@github.com>
2018-04-07 20:44:28 -0400

    First Commit

---
+++ ek.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+#
+# It's to find out max flow in a flow network
+# It's similar to Ford-Fulkerson algorithm,
+# but defines search order for augmenting path
+# The path found must be a shortest path that has available capacity.
+# http://en.wikipedia.org/wiki/Edmonds%E2%80%93Karp_algorithm
+#
+
+
+class Graph:
+
+    def __init__(self):
+        self.node = []
+        self.edge = {}
+        self.neighbors = {}
+        self.graph = []  # residual graph
+        self.residual = []
+        self.row = None
+        self.source = None
+        self.sink = None
+        self.sset = None
+        self.tset = None
+
+    # add nodes
+    def add_node(self, node=[]):
+        self.node = node
+
+    # add edges
+    def add_edge(self, node=(), capacity=None):
+        self.edge.setdefault(node, capacity)
+
+    # build the network flow
+    def build_flow(self, source=None, sink=None):
+        for i in range(len(self.node)):
+            self.graph.append([])
+            self.graph[i] = [0 for j in range(len(self.node))]
+            self.neighbors.setdefault(i, [])
+        for i, j in self.edge.keys():
+            self.graph[i][j] = self.edge[(i, j)]
+            self.neighbors[i].append(j)
+            if i not in self.neighbors[j]:
+                self.neighbors[j].append(i)
+        self.residual = [i[:] for i in self.graph]
+        self.row = len(self.graph)
+        self.source = source
+        self.sink = sink
+
+    def edmonds_karp(self):
+        flow = 0
+        length = len(self.graph)
+        flows = [[0 for i in range(length)] for j in range(length)]
+        while True:
+            max, parent = self.bfs(flows)
+            print(max)
+            if max == 0:
+                self.sset = [self.source] + [i for i, v in enumerate(parent) if v >= 0]
+                self.tset = [x for x in self.node if x not in self.sset]
+                print(self.sset, self.tset)
+                break
+            flow = flow + max
+            v = self.sink
+            while v != self.source:
+                u = parent[v]
+                flows[u][v] = flows[u][v] + max
+                self.residual[u][v] -= max
+                flows[v][u] = flows[v][u] - max
+                self.residual[v][u] += max
+                v = u
+        return flow, flows
+
+    def bfs(self, flows):
+        length = self.row
+        parents = [-1 for i in range(length)]  # parent table
+        parents[self.source] = -2  # make sure source is not rediscovered
+        M = [0 for i in range(length)]  # Capacity of path to vertex i
+        M[self.source] = float('Inf')  # this is necessary!
+
+        queue = []
+        queue.append(self.source)
+        while queue:
+            u = queue.pop(0)
+            for v in self.neighbors[u]:
+                # if there is available capacity and v is is not seen before in
+                # search
+                if self.graph[u][v] - flows[u][v] > 0 and parents[v] == -1:
+                    parents[v] = u
+                    # it will work because at the beginning M[u] is Infinity
+                    M[v] = min(M[u], self.graph[u][v] - flows[u]
+                               [v])  # try to get smallest
+                    if v != self.sink:
+                        queue.append(v)
+                    else:
+                        return M[self.sink], parents
+        return 0, parents
+
+    def find_cut(self):
+        cut = {}
+        for i in self.sset:
+            cut[i] = 0
+        for i in self.tset:
+            cut[i] = 1
+        return cut
+
+
+def test():
+    g = Graph()
+    g.add_node(list(range(7)))
+    g.add_edge((0, 1), 3)
+    g.add_edge((0, 3), 3)
+    g.add_edge((1, 2), 4)
+    g.add_edge((2, 0), 3)
+    g.add_edge((2, 3), 1)
+    g.add_edge((2, 4), 2)
+    g.add_edge((3, 4), 2)
+    g.add_edge((3, 5), 6)
+    g.add_edge((4, 1), 1)
+    g.add_edge((4, 6), 1)
+    g.add_edge((5, 6), 9)
+    g.build_flow(0, 6)
+    for line in g.graph:
+        print(line)
+    print(g.neighbors)
+    flow, flows = g.edmonds_karp()
+    print('Max flow:', flow)
+    cut = g.find_cut()
+    print('Min cut:', cut)
---
+++ main_kmean.py
@@ -0,0 +1,162 @@
+from scipy.misc import imresize
+import cv2
+import numpy as np
+import time
+import sys
+from ek import Graph
+from bayes import BayesClassifier
+from sklearn.mixture import GaussianMixture
+import matplotlib.pyplot as plt
+
+
+def build_bayes_graph(img, labels, sigma=1e2, kappa=2):
+    """ Build a graph from 4-neighborhood of pixels.
+        Foreground and background is determined from
+        labels (1 for foreground, -1 for background, 0 otherwise)
+        and is modeled with naive Bayes classifiers."""
+    m, n = img.shape[:2]
+    # RGB vector version (one pixel per row)
+    vim = img.reshape((-1, 3))
+    # RGB for foreground and background
+    foreground = img[labels == 1].reshape((-1, 3))
+    background = img[labels == 0].reshape((-1, 3))
+    train_data = [foreground, background]
+    # train naive Bayes classifier
+    bc = BayesClassifier()
+    bc.train(train_data)
+    # get probabilities for all pixels
+    bc_lables, prob = bc.classify(vim)
+    prob_fg, prob_bg = prob[0], prob[1]
+    print(np.amax(prob_fg), np.max(prob_bg))
+    # create graph with m*n+2 nodes
+    gr = Graph()
+    gr.add_node(range(m * n + 2))
+    source = m * n  # second to last is source
+    sink = m * n + 1  # last node is sink
+    # normalize
+    for i in range(vim.shape[0]):
+        vim[i] = vim[i] / np.linalg.norm(vim[i])
+    # go through all nodes and add edges
+    for i in range(m * n):
+        print(i)
+        # add edge from source
+        gr.add_edge((source, i), (prob_fg[i] / (prob_fg[i] + prob_bg[i])))
+        # add edge to sink
+        gr.add_edge((i, sink), (prob_bg[i] / (prob_fg[i] + prob_bg[i])))
+        # add edges to neighbors
+        if i % n != 0:  # left exists
+            edge_wt = kappa * \
+                np.exp(-1.0 * sum((vim[i] - vim[i - 1])**2) / sigma)
+            gr.add_edge((i, i - 1), edge_wt)
+        if (i + 1) % n != 0:  # right exists
+            edge_wt = kappa * \
+                np.exp(-1.0 * sum((vim[i] - vim[i + 1])**2) / sigma)
+            gr.add_edge((i, i + 1), edge_wt)
+        if i // n != 0:  # up exists
+            edge_wt = kappa * \
+                np.exp(-1.0 * sum((vim[i] - vim[i - n])**2) / sigma)
+            gr.add_edge((i, i - n), edge_wt)
+        if i // n != m - 1:  # down exists
+            edge_wt = kappa * \
+                np.exp(-1.0 * sum((vim[i] - vim[i + n])**2) / sigma)
+            gr.add_edge((i, i + n), edge_wt)
+    gr.build_flow(source, sink)
+    return gr
+
+
+def cut_graph(gr, imsize):
+    h, w = imsize
+    flows = gr.edmonds_karp()
+    cuts = gr.find_cut()
+    # convert graph to image with labels
+    res = np.zeros(h * w)
+    for pos in list(cuts.keys())[1:-1]:  # don't add source/sink
+        res[pos - 1] = cuts[pos]
+    return res.reshape((h, w))
+
+
+def create_msr_labels(anno):
+    """ Create label matrix for training from
+        user annotations. """
+    labels = np.zeros(anno.shape[:2])
+
+    anno_hsv=cv2.cvtColor(anno, cv2.COLOR_BGR2HSV)
+
+    # lower red mask (0-10)
+    lower_red = np.array([0,43,46])
+    upper_red = np.array([10,255,255])
+    mask0 = cv2.inRange(anno_hsv, lower_red, upper_red)
+
+    # upper red mask (170-180)
+    lower_red = np.array([156,43,46])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(anno_hsv, lower_red, upper_red)
+
+    # join red masks
+    red_mask = mask0+mask1
+
+    # blue mask
+    lower_blue = np.array([100,43,46])
+    upper_blue = np.array([124,255,255])
+    blue_mask = cv2.inRange(anno_hsv, lower_blue, upper_blue)
+
+    # background
+    labels[np.where(red_mask>0)] = -1
+    # foreground
+    labels[np.where(blue_mask>0)] = 1
+
+    return labels
+
+
+def graph_cuts(img, scale):
+    img_down = imresize(img, scale, interp='bilinear')  # downsample
+    # anno_down = imresize(anno, scale, interp='nearest') # downsample
+    size = img_down.shape[:2]
+    vim = img_down.reshape((-1, 3)).astype('float32')
+    # create label
+    # labels = create_msr_labels(anno_down)
+    '''
+    gmm = GaussianMixture(n_components=2, covariance_type='tied', max_iter=1)
+    vim = img_down.reshape((-1, 3))
+    gmm = gmm.fit(vim)
+    cluster = gmm.predict(vim)
+    cluster = cluster.reshape(img_down.shape[0], img_down.shape[1]).astype('float64')
+    print(cluster)
+    labels = np.zeros((cluster.shape[0], cluster.shape[1])).astype('float64')
+    labels[cluster==0] = -1
+    labels[cluster==1] = 1
+    '''
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret, labels, center=cv2.kmeans(vim,2,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    labels = labels.reshape((size))
+    print(labels)
+    # create graph
+    g = build_bayes_graph(img_down, labels, sigma=1e20, kappa=1)
+    # cut the graph
+    mask = cut_graph(g, size)
+    mask = cv2.resize(mask, (img.shape[1], img.shape[0]))  # upsample
+    mask = np.array(mask, dtype=np.uint8)
+    back = cv2.bitwise_and(img, img, mask=mask)
+    fore = img - back
+    return mask, fore, back
+
+
+if __name__ == '__main__':
+    scale = 0.25
+    img_name = sys.argv[1]
+    img = cv2.imread(img_name)
+    # anno = cv2.imread(anno_name)
+    start_time = time.time()
+    mask, fore, back = graph_cuts(img, scale)
+    end_time = time.time()
+    total = end_time - start_time
+    print('Running time: {:2}s'.format(total))
+    plt.figure()
+    plt.imshow(img)
+    plt.figure()
+    plt.imshow(mask)
+    plt.figure()
+    plt.imshow(fore)
+    plt.figure()
+    plt.imshow(back)
+    plt.show()
